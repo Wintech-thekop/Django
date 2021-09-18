bc968bc76e56224f063b468a31163f01c75226b8
wintech <wintech_thekop@hotmail.com>
2021-09-18 21:13:38 +0700

    Uncle Engineer Django EP3

--- company/views.py
+++ company/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def Home(request):
-    return HttpResponse('<h1>Hello World!!</h1> <br> <p>by Wintech</p>')
+# def Home(request):
+#     return HttpResponse('<h1>Hello World!!</h1> <br> <p>by Wintech</p>')
     
+def Home(request):
+    return render(request, 'company/home.html')
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rg4@f6l%vk$0zkl=7qd(ng%z)0*4^#vy3n$++l=m#=gg5*q#$7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'company/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
